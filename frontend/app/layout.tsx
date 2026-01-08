397e0887e08106f33533fb720f42ba38a7de3d98
28ad <alessiodita10@gmail.com>
2026-01-08 19:54:43 +0000

    added landing page

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={lato.variable}>
+      
       <body>
 
         {children}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,38 @@
+import heroImg from './assets/images/hero-image.png';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans bg-white">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
+    <div className="h-screen bg-white">
+
+      {/* hero section */}
+      <main className="p-8 lg:h-3/4 w-full bg-base-black flex justify-center items-center shadow-md">
         
-        <div className="text-black font-bold">ttt</div>
+        {/* hero container */}
+        <div className=" p-6 lg:h-full w-full border-2 border-white rounded-xl flex md:justify-evenly">
+
+          {/* hero main text + call to action */}
+
+          <div className="flex flex-col items-start justify-center md:w-1/2">
+            <div className="text-white text-3xl lg:text-6xl font-extrabold">TRACK ALL YOUR VEHICLE'S MAINTENANCE IN ONE PLACE</div>
+            <div className="text-white text-md mt-2">Join now and keep track of your car's service history and much more in a few clicks</div>
+            <button className="text-white font-bold mt-2 p-2 border-[0.5px] border-white rounded-lg cursor-pointer hover:bg-primary-accent hover:border-none duration-200 ease-in-out">GET STARTED</button>
+          </div>
+
+          <div className="hidden md:flex">
+            <Image 
+            src={heroImg} 
+            alt='Hero image of a car'
+            className='object-contain'/>
+          </div>
+
+
+        </div>
         
       </main>
+
+      {/* services sections */}
+      
     </div>
   );
 }
